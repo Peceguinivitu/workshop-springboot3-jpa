fd40186a039483b7b3685f54d1e60e624109aafb
Peceguinivitor <vitorgomespeceguini0@gmail.com>
2025-03-24 18:29:17 -0300

    Exception handling - findById

--- src/main/java/com/peceguini/course/services/UserService.java
+++ src/main/java/com/peceguini/course/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.peceguini.course.entities.User;
 import com.peceguini.course.repositories.UserRepository;
+import com.peceguini.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	}
    public User findById(Long  id) {
 	   Optional<User> obj = repository.findById(id);
-	   return obj.get();
+	   return obj.orElseThrow(() -> new ResourceNotFoundException(id));
    }
    public User insert(User obj) {
 	   return repository.save(obj);
